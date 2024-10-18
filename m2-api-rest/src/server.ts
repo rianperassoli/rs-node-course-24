4408affb34bb3b4e046b9a8899d963250d3e3a0a
Rian Perassoli <rian.perassoli@gmail.com>
2024-10-18 10:19:11 -0300

    creating env vars and validating them

--- m2-api-rest/src/server.ts
+++ m2-api-rest/src/server.ts
@@ -1,12 +1,15 @@
 import fastify from "fastify";
 import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
 app.get("/hello", async () => {
-  const tables = await knex("sqlite_schema").select("*");
+  const transactions = await knex("transactions").select("*");
 
-  return tables;
+  return transactions;
 });
 
-app.listen({ port: 3333 }).then(() => console.log("HTTP Server is running!"));
+app
+  .listen({ port: env.PORT })
+  .then(() => console.log("HTTP Server is running!"));
