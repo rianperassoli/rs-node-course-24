540f5ef76196194cedf0d9d27133b088bd32c218
Rian Perassoli <rian.perassoli@gmail.com>
2024-10-23 10:57:06 -0300

    created test of post and get

---
+++ m2-api-rest/test/transactions.spec.ts
@@ -0,0 +1,48 @@
+import { beforeAll, afterAll, describe, it, expect } from "vitest";
+import supertest from "supertest";
+import { app } from "../src/app";
+
+describe("Transaction Routes", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to create a new transaction", async () => {
+    await supertest(app.server)
+      .post("/transactions")
+      .send({
+        title: "test",
+        amount: 5000,
+        type: "credit",
+      })
+      .expect(201);
+  });
+
+  it("should be able to list all transactions", async () => {
+    const createTransactionResponse = await supertest(app.server)
+      .post("/transactions")
+      .send({
+        title: "New transaction",
+        amount: 5000,
+        type: "credit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie") || [];
+
+    const listTransactionsResponse = await supertest(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "New transaction",
+        amount: 5000,
+      }),
+    ]);
+  });
+});
