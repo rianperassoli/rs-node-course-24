ea6140858eadf0fa954d37658e92bb35038ad742
Rian Perassoli <rian.perassoli@gmail.com>
2024-10-25 10:23:14 -0300

    finishing tests

--- m2-api-rest/test/transactions.spec.ts
+++ m2-api-rest/test/transactions.spec.ts
@@ -1,4 +1,5 @@
-import { beforeAll, afterAll, describe, it, expect } from "vitest";
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from "vitest";
+import { execSync } from "node:child_process";
 import supertest from "supertest";
 import { app } from "../src/app";
 
@@ -9,6 +10,11 @@ describe("Transaction Routes", () => {
 
   afterAll(async () => {
     await app.close();
+  });
+
+  beforeEach(() => {
+    execSync("npm run knex migrate:rollback --all");
+    execSync("npm run knex migrate:latest");
   });
 
   it("should be able to create a new transaction", async () => {
@@ -44,5 +50,57 @@ describe("Transaction Routes", () => {
         amount: 5000,
       }),
     ]);
+  });
+
+  it("should be able to get a specific transaction", async () => {
+    const createTransactionResponse = await supertest(app.server)
+      .post("/transactions")
+      .send({
+        title: "New transaction",
+        amount: 5000,
+        type: "credit",
+      });
+    const cookies = createTransactionResponse.get("Set-Cookie") || [];
+    const listTransactionsResponse = await supertest(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+    const transactionId = listTransactionsResponse.body.transactions[0].id;
+    const getTransactionResponse = await supertest(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set("Cookie", cookies)
+      .expect(200);
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: "New transaction",
+        amount: 5000,
+      })
+    );
+  });
+
+  it("should be able to get the summary", async () => {
+    const createTransactionResponse = await supertest(app.server)
+      .post("/transactions")
+      .send({
+        title: "Credit transaction",
+        amount: 5000,
+        type: "credit",
+      });
+    const cookies = createTransactionResponse.get("Set-Cookie") || [];
+    await supertest(app.server)
+      .post("/transactions")
+      .set("Cookie", cookies)
+      .send({
+        title: "Debit transaction",
+        amount: 2000,
+        type: "debit",
+      });
+    const summaryResponse = await supertest(app.server)
+      .get("/transactions/summary")
+      .set("Cookie", cookies)
+      .expect(200);
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 3000,
+    });
   });
 });
