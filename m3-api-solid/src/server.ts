312b8bdcc894f0896bfe3f379b5c873d76f207d8
Rian Perassoli <rian.perassoli@gmail.com>
2024-11-01 09:30:18 -0300

    creating env settings

--- m3-api-solid/src/server.ts
+++ m3-api-solid/src/server.ts
@@ -1,5 +1,6 @@
 import { app } from "./app";
+import { env } from "./env";
 
-app.listen({ host: "0.0.0.0", port: 3333 }).then(() => {
-  console.log("🚀 HTTP Server is running!");
+app.listen({ host: "0.0.0.0", port: env.PORT }).then(() => {
+  console.log(`🚀 HTTP Server is running on PORT:${env.PORT}!`);
 });
