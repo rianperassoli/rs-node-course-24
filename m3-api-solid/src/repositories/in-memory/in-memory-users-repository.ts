e266ebc58412da485707b975571f86de06684bba
Rian Perassoli <rian.perassoli@gmail.com>
2024-11-22 11:03:21 -0300

    created check-in use case

--- m3-api-solid/src/repositories/in-memory/in-memory-users-repository.ts
+++ m3-api-solid/src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,17 +1,18 @@
-import { UsersRepository } from "@/repositories/users-repository";
+import { randomUUID } from "node:crypto";
 import { User, Prisma } from "@prisma/client";
+import { UsersRepository } from "@/repositories/users-repository";
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = [];
 
   async findById(id: string) {
-    const user = this.items.find((item) => item.id === id)
+    const user = this.items.find((item) => item.id === id);
 
     if (!user) {
-      return null
+      return null;
     }
 
-    return user
+    return user;
   }
 
   async findByEmail(email: string) {
@@ -26,7 +27,7 @@ export class InMemoryUsersRepository implements UsersRepository {
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: "user-1",
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
