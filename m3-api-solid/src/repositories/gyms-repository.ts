4e23a7437cf5cab44557675e666358d028c8b134
Rian Perassoli <rian.perassoli@gmail.com>
2024-11-25 14:35:25 -0300

    created fetch-nearby use case

--- m3-api-solid/src/repositories/gyms-repository.ts
+++ m3-api-solid/src/repositories/gyms-repository.ts
@@ -1,7 +1,13 @@
 import { Gym, Prisma } from "@prisma/client";
 
+export interface FindManyNearbyParams {
+  latitude: number
+  longitude: number
+}
+
 export interface GymsRepository {
   searchMany(query: string, page: number): Promise<Gym[]>;
+  findManyNearby(params: FindManyNearbyParams): Promise<Gym[]>
   findById(id: string): Promise<Gym | null>;
   create(data: Prisma.GymCreateInput): Promise<Gym>;
 }
