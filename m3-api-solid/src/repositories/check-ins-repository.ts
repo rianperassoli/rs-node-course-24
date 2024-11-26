141517e7c2beacd33d82d9c6d6497bdab927f926
Rian Perassoli <rian.perassoli@gmail.com>
2024-11-26 11:24:06 -0300

    created validate-check-in use case

--- m3-api-solid/src/repositories/check-ins-repository.ts
+++ m3-api-solid/src/repositories/check-ins-repository.ts
@@ -2,7 +2,9 @@ import { CheckIn, Prisma } from "@prisma/client";
 
 export interface CheckInsRepository {
   countByUserId(userId: string): Promise<number>;
+  findById(id: string): Promise<CheckIn | null>;
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
+  save(checkIn: CheckIn): Promise<CheckIn>;
 }
