62bfb79defa9acb353c712a1e9215a0d0c5557fb
Rian Perassoli <rian.perassoli@gmail.com>
2024-12-04 14:13:09 -0300

    separing routes from gymms and user and finishing creating gyms' controllers

---
+++ m3-api-solid/src/http/controllers/gyms/routes.ts
@@ -0,0 +1,14 @@
+import { FastifyInstance } from "fastify";
+import { verifyJwt } from "@/http/middlewares/verify-jwt";
+
+import { create } from "./create";
+import { search } from "./search";
+import { nearby } from "./nearby";
+
+export async function gymsRoutes(app: FastifyInstance) {
+  app.addHook("onRequest", verifyJwt);
+
+  app.post("/gyms", create);
+  app.get("/gyms/search", search);
+  app.get("/gyms/nearby", nearby);
+}
--- m3-api-solid/src/http/controllers/users/routes.ts
+++ m3-api-solid/src/http/controllers/users/routes.ts
@@ -1,11 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { verifyJwt } from "@/http/middlewares/verify-jwt";
 
-import { register } from "@/http/controllers/register";
-import { authenticate } from "@/http/controllers/authenticate";
-import { profile } from "@/http/controllers/profile";
+import { authenticate } from "./authenticate";
+import { profile } from "./profile";
+import { register } from "./register";
 
-export async function appRoutes(app: FastifyInstance) {
+export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
 
