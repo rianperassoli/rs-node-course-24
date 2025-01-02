117675d47387e181175eb87059b9d2b75dceef05
Rian Perassoli <rian.perassoli@gmail.com>
2025-01-02 10:28:36 -0300

    creating slug value-object

--- m4-clean-ddd/src/domain/entities/question.ts
+++ m4-clean-ddd/src/domain/entities/question.ts
@@ -1,9 +1,11 @@
 import { randomUUID } from "node:crypto";
+import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
   title: string;
   content: string;
   authorId: string;
+  slug: Slug;
 }
 
 export class Question {
@@ -11,13 +13,15 @@ export class Question {
   public title: string;
   public content: string;
   public authorId: string;
+  public slug: Slug;
 
   constructor(props: QuestionProps, id?: string) {
-    const { title, content, authorId } = props;
+    const { title, content, authorId, slug } = props;
 
     this.id = id ?? randomUUID();
     this.title = title;
     this.content = content;
     this.authorId = authorId;
+    this.slug = slug;
   }
 }
